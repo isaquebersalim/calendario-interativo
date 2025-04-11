d3c16c64390ab615da6a4c40075503c4e3e1ab63
Isaque Bersalim <isaquebersalim@gmail.com>
2025-04-11 12:24:49 -0300

    Criando o arquivo de JavaScript, e adcionando a função de colorir o dia, em que o usuário escolhe um dia e uma cor

---
+++ script.js
@@ -0,0 +1,10 @@
+function colorirDia(){
+    // Pegar valor do campo dia
+    let days = document.getElementById('days').value;
+
+    // Pegar o valor do campo de cor
+    let color = document.getElementById('color').value;
+
+    // Pega os elementos da tabela calendário
+    let calendar = document.getElementById('calendar');
+}
